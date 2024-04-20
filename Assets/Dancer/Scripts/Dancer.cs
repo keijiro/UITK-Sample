8e606848d521720d7ceede250825c8394ad46246
Keijiro Takahashi <keijiro@gmail.com>
2024-04-20 18:10:15 +0900

    Dancer

---
+++ Assets/Dancer/Scripts/Dancer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public sealed class Dancer : MonoBehaviour
+{
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        var anim = GetComponent<Animator>();
+        root.Q<Button>("button-dance").clicked += () => anim.Play("Dance");
+        root.Q<Button>("button-spin") .clicked += () => anim.Play("Spin");
+        root.Q<Button>("button-flip") .clicked += () => anim.Play("Flip");
+    }
+}
